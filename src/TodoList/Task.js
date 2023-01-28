b0e5ebc7ab782a9dfc5562626613c284869c9e96
davidrm1997 <53543168+David010997@users.noreply.github.com>
2023-01-29 00:31:01 +0400

    added task create function

--- src/TodoList/Task.js
+++ src/TodoList/Task.js
@@ -1,12 +1,15 @@
 import { TaskBlock, TaskCreateBtn, TaskForm, TaskInput, TaskTypeCount } from "./style";
 
 function Task() {
+    function addTask() {
+        console.log('task');
+    }
     return (
         <TaskBlock>
             <h2>Todo List</h2>
             <TaskForm>
                 <TaskInput />
-                <TaskCreateBtn>Add Task</TaskCreateBtn>
+                <TaskCreateBtn onClick={addTask}>Add Task</TaskCreateBtn>
                 <TaskTypeCount>Pending Tasks Count</TaskTypeCount>
             </TaskForm>
 
